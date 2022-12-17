3d917c4e68264f900c7d21ebd33f0d61315bd04a
mandy1223ks <mandy1223ks@gmail.com>
2022-12-17 18:55:24 +0800

    fight

--- hpe.py
+++ hpe.py
@@ -20,12 +20,17 @@ with mp_pose.Pose(
     action1 = [False, False, False, False, False] # raise left hand, raise right hand, punch left, punch right, defense for left preson
     action2 = [False, False, False, False, False] # right person
     
+    
+    
     l_cnt, r_cnt = 0, 0
 
     while True:
         l_cnt += 1
         r_cnt += 1
-
+        
+        fighter_1 = [False,False,False,False,False]
+        fighter_2 = [False,False,False,False,False]
+        
         ret, img = cap.read()
         if not ret:
             print("Cannot receive frame")
@@ -45,76 +50,88 @@ with mp_pose.Pose(
             for i in range(33):
                 results_1.pose_landmarks.landmark[i].x /=2
                 # print(f'person 1: {i} = {results_1.pose_landmarks.landmark[i].x}')
-                left_hand_x, left_hand_y = results_1.pose_landmarks.landmark[15].x, results_1.pose_landmarks.landmark[15].y
-                right_hand_x, right_hand_y = results_1.pose_landmarks.landmark[16].x, results_1.pose_landmarks.landmark[16].y
-                nose_x, nose_y = results_1.pose_landmarks.landmark[0].x, results_1.pose_landmarks.landmark[0].y
-                # if left_hand_y < nose_y and action1[0] == False:
-                #     action1[0] = True
-                #     print('left person raising right hand')
-                # elif left_hand_y >= nose_y and action1[0] == True:
-                #     action1[0] = False
-                # elif right_hand_y < nose_y and action1[1] == False:
-                #     action1[1] = True
-                #     print('left person raising left hand')
-                # elif right_hand_y >= nose_y and action1[1] == True:
-                #     action1[1] = False
-                # elif nose_x - left_hand_x > 0.3 and action1[2] == False:  # this num hasn't been tested
-                #     action1[2] = True
-                #     print('left person punch left')
-                # # TODO : punch left restart
-                # elif right_hand_x - nose_x > 0.3 and action1[3] == False:  # this num hasn't been tested
-                #     action1[3] == True
-                #     print('left person punch right')
-                # # TODO : punch right restart
-                # elif abs(left_hand_x-right_hand_x) < 0.1 and action1[4] == False: # this num hasn't been tested
-                #     action1[4] == True
-                #     print('defense')
-                # TODO : defense restart
-
-                # else:
-                #     print('L no action')
+            left_hand_x, left_hand_y = results_1.pose_landmarks.landmark[15].x, results_1.pose_landmarks.landmark[15].y
+            right_hand_x, right_hand_y = results_1.pose_landmarks.landmark[16].x, results_1.pose_landmarks.landmark[16].y
+            nose_x, nose_y = results_1.pose_landmarks.landmark[0].x, results_1.pose_landmarks.landmark[0].y
+            # if left_hand_y < nose_y and action1[0] == False:
+            #     action1[0] = True
+            #     print('left person raising right hand')
+            # elif left_hand_y >= nose_y and action1[0] == True:
+            #     action1[0] = False
+            # elif right_hand_y < nose_y and action1[1] == False:
+            #     action1[1] = True
+            #     print('left person raising left hand')
+            # elif right_hand_y >= nose_y and action1[1] == True:
+            #     action1[1] = False
+            # elif nose_x - left_hand_x > 0.3 and action1[2] == False:  # this num hasn't been tested
+            #     action1[2] = True
+            #     print('left person punch left')
+            # # TODO : punch left restart
+            # elif right_hand_x - nose_x > 0.3 and action1[3] == False:  # this num hasn't been tested
+            #     action1[3] = True
+            #     print('left person punch right')
+            # # TODO : punch right restart
+            # elif abs(left_hand_x-right_hand_x) < 0.1 and action1[4] == False: # this num hasn't been tested
+            #     action1[4] = True
+            #     print('defense')
+            # TODO : defense restart
+            # else:
+            #     print('L no action')
                 
         results_2 = pose.process(crop_img_2)
         if results_2.pose_landmarks is not None:
-            print(abs(right_hand_x - nose_x))
+            # print(abs(right_hand_x - nose_x))
             for i in range(33):
                 results_2.pose_landmarks.landmark[i].x = (results_2.pose_landmarks.landmark[i].x)/2 + 0.5
                 # print(f'person 2: {i} = {results_2.pose_landmarks.landmark[i].x}')
-                left_hand_x, left_hand_y = results_2.pose_landmarks.landmark[15].x, results_2.pose_landmarks.landmark[15].y
-                right_hand_x, right_hand_y = results_2.pose_landmarks.landmark[16].x, results_2.pose_landmarks.landmark[16].y
-                nose_x, nose_y = results_2.pose_landmarks.landmark[0].x, results_2.pose_landmarks.landmark[0].y
-                if r_cnt >= 5:
-                    if left_hand_y < nose_y and action2[0] == False:
-                        action2[0] = True
-                        print('right person raising right hand')
-                        r_cnt = 0
-                    elif left_hand_y >= nose_y and action2[0] == True:
-                        action2[0] = False
-                    elif right_hand_y < nose_y and action2[1] == False:
-                        action2[1] = True
-                        print('right person raising left hand')
-                        r_cnt = 0
-                    elif right_hand_y >= nose_y and action2[1] == True:
-                        action2[1] = False
-                    elif nose_x - left_hand_x > 0.3 and action2[2] == False:  # this num hasn't been tested
-                        action2[2] = True
-                        print('right person punch left')
-                        r_cnt = 0
-                    # TODO : punch left restart
-                    elif abs(right_hand_x - nose_x) > 0.5 and action2[3] == False:  # this num hasn't been tested
-                        action2[3] == True
-                        print('right person punch right')
-                        r_cnt = 0
-                    # TODO : punch right restart
-                    elif abs(left_hand_x - right_hand_x) < 0.1 and action2[4] == False: # this num hasn't been tested
-                        action2[4] == True
-                        print('defense')
-                    else:
-                        print('None')
-                        for i in range(len(action2)):
-                            action2[i] = False
+            left_hand_x, left_hand_y = results_2.pose_landmarks.landmark[15].x, results_2.pose_landmarks.landmark[15].y
+            right_hand_x, right_hand_y = results_2.pose_landmarks.landmark[16].x, results_2.pose_landmarks.landmark[16].y
+            nose_x, nose_y = results_2.pose_landmarks.landmark[0].x, results_2.pose_landmarks.landmark[0].y
+            if r_cnt >= 5:
+                r_cnt = 0
+                # right person raising right hand
+                if left_hand_y < nose_y and action2[0] == False:
+                    action2[0], fighter_2[0] = True, True
+                    print('right person raising right hand')
+                    # r_cnt = 0
+                elif left_hand_y >= nose_y and action2[0] == True:
+                    action2[0] = False
+                    print('None 0')
                         
-                    # TODO : defense restart
+                # right person raising left hand    
+                elif right_hand_y < nose_y and action2[1] == False:
+                    action2[1], fighter_2[1] = True, True
+                    print('right person raising left hand')
+                    # r_cnt = 0
+                elif right_hand_y >= nose_y and action2[1] == True:
+                    action2[1] = False
+                    print('None 1')
+                    
+                # right person punch left    
+                elif nose_x - left_hand_x > 0.3 and action2[2] == False:  # this num hasn't been tested
+                    action2[2], fighter_2[2] = True, True
+                    print('right person punch left')
+                    # r_cnt = 0
+
+                # right person punch right
+                elif abs(right_hand_x - nose_x) > 0.5 and action2[3] == False:  # this num hasn't been tested
+                    action2[3], fighter_2[3] = True, True
+                    print('right person punch right')
+                    # r_cnt = 0
+                        
+                # defense
+                elif abs(left_hand_x - right_hand_x) < 0.1 and action2[4] == False: # this num hasn't been tested
+                    action2[4], fighter_2[4] = True, True
+                    print('defense')
+                    
+                # no action   
+                else:
+                    print('None')
+                    action2[2], action2[3], action2[4] = False, False, False
+                    # for i in range(len(action2)):
+                    #     action2[i] = False
+                        
+
         sleep(.1)
 
         

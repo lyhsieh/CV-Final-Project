41c634b9489cdece10265ad57cc246259eef8952
LeoTheBestCoder <leoshieh0hn@gmail.com>
2022-12-17 16:02:32 +0800

    fix: y position

--- hpe.py
+++ hpe.py
@@ -43,27 +43,27 @@ with mp_pose.Pose(
                 left_hand_x, left_hand_y = results_1.pose_landmarks.landmark[15].x, results_1.pose_landmarks.landmark[15].y
                 right_hand_x, right_hand_y = results_1.pose_landmarks.landmark[16].x, results_1.pose_landmarks.landmark[16].y
                 nose_x, nose_y = results_1.pose_landmarks.landmark[0].x, results_1.pose_landmarks.landmark[0].y
-                if left_hand_y < nose_y and action1[0] == False:
-                    action1[0] = True
-                    print('left person raising right hand')
-                elif left_hand_y <= nose_y and action1[0] == True:
-                    action1[0] = False
-                elif right_hand_y < nose_y and action1[1] == False:
-                    action1[1] = True
-                    print('left person raising left hand')
-                elif right_hand_y <= nose_y and action1[1] == True:
-                    action1[1] = False
-                elif nose_x - left_hand_x > 0.3 and action1[2] == False:  # this num hasn't been tested
-                    action1[2] = True
-                    print('left person punch left')
-                # TODO : punch left restart
-                elif right_hand_x - nose_x > 0.3 and action1[3] == False:  # this num hasn't been tested
-                    action1[3] == True
-                    print('left person punch right')
-                # TODO : punch right restart
-                elif abs(left_hand_x-right_hand_x) < 0.1 and action1[4] == False: # this num hasn't been tested
-                    action1[4] == True
-                    print('defense')
+                # if left_hand_y < nose_y and action1[0] == False:
+                #     action1[0] = True
+                #     print('left person raising right hand')
+                # elif left_hand_y >= nose_y and action1[0] == True:
+                #     action1[0] = False
+                # elif right_hand_y < nose_y and action1[1] == False:
+                #     action1[1] = True
+                #     print('left person raising left hand')
+                # elif right_hand_y >= nose_y and action1[1] == True:
+                #     action1[1] = False
+                # elif nose_x - left_hand_x > 0.3 and action1[2] == False:  # this num hasn't been tested
+                #     action1[2] = True
+                #     print('left person punch left')
+                # # TODO : punch left restart
+                # elif right_hand_x - nose_x > 0.3 and action1[3] == False:  # this num hasn't been tested
+                #     action1[3] == True
+                #     print('left person punch right')
+                # # TODO : punch right restart
+                # elif abs(left_hand_x-right_hand_x) < 0.1 and action1[4] == False: # this num hasn't been tested
+                #     action1[4] == True
+                #     print('defense')
                 # TODO : defense restart
 
                 # else:
@@ -80,12 +80,12 @@ with mp_pose.Pose(
                 if left_hand_y < nose_y and action2[0] == False:
                     action2[0] = True
                     print('right person raising right hand')
-                elif left_hand_y <= nose_y and action2[0] == True:
+                elif left_hand_y >= nose_y and action2[0] == True:
                     action2[0] = False
                 elif right_hand_y < nose_y and action2[1] == False:
                     action2[1] = True
                     print('right person raising left hand')
-                elif right_hand_y <= nose_y and action2[1] == True:
+                elif right_hand_y >= nose_y and action2[1] == True:
                     action2[1] = False
                 elif nose_x - left_hand_x > 0.3 and action2[2] == False:  # this num hasn't been tested
                     action2[2] = True
@@ -95,10 +95,11 @@ with mp_pose.Pose(
                     action2[3] == True
                     print('right person punch right')
                 # TODO : punch right restart
-                elif abs(left_hand_x-right_hand_x) < 0.1 and action2[4] == False: # this num hasn't been tested
+                elif abs(left_hand_x - right_hand_x) < 0.1 and action2[4] == False: # this num hasn't been tested
                     action2[4] == True
                     print('defense')
                 # TODO : defense restart
+        sleep(.05)
 
         
 

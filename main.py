9bf3f78504590c688913bea5868d05cf434bec7a
LeoTheBestCoder <leoshieh0hn@gmail.com>
2022-12-17 23:13:32 +0800

    feat: first integration

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ from fighter import Fighter
 import os
 import natsort
 
+
+# human pose libraries ####################################################
+import cv2
+import mediapipe as mp
+from time import sleep
+
+mp_drawing = mp.solutions.drawing_utils          # mediapipe 繪圖方法
+mp_drawing_styles = mp.solutions.drawing_styles  # mediapipe 繪圖樣式
+mp_pose = mp.solutions.pose                      # mediapipe pose estimation
+cap = cv2.VideoCapture(0)
+############################################################################
+
 mixer.init()
 pygame.init()
 
@@ -135,76 +147,257 @@ fighter_2 = Fighter(2, 700, 310, True, P2_DATA,
 
 # game loop
 run = True
-while run:
 
-    P1_action = [False, False, False, False, False]  # bool list
-    P2_action = [False, False, False, False, False]
+l_cnt, r_cnt = 0, 0
+PUNCH_TH = 0.2
 
-    # ⬆︎ human pose estimation ⬆︎
+with mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as pose:
 
-    clock.tick(FPS)
+    action1 = [False, False, False, False, False] # raise left hand, raise right hand, punch left, punch right, defense for left preson
+    action2 = [False, False, False, False, False] # right person
 
-    # draw background
-    draw_bg()
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
 
-    # show player stats
-    draw_health_bar(fighter_1.health, 20, 20)
-    draw_health_bar(fighter_2.health, 580, 20)
-    draw_text("P1: " + str(score[0]), score_font, RED, 20, 60)
-    draw_text("P2: " + str(score[1]), score_font, RED, 580, 60)
+    while run:
+        l_cnt += 1
+        r_cnt += 1
 
-    # update countdown
-    if intro_count <= 0:
-        # move fighters
-        fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT,
-                       screen, fighter_2, round_over, P1_action)
-        fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT,
-                       screen, fighter_1, round_over, P2_action)
-    else:
-        # display count timer
-        draw_text(str(intro_count), count_font, RED,
-                  SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
-        # update count timer
-        if (pygame.time.get_ticks() - last_count_update) >= 1000:
-            intro_count -= 1
-            last_count_update = pygame.time.get_ticks()
+        person_1 = [False, False, False, False, False]
+        person_2 = [False, False, False, False, False]
 
-    # update fighters
-    fighter_1.update()
-    fighter_2.update()
+        ret, img = cap.read()
+        if not ret:
+            print("Cannot receive frame")
+            break
+        
+        img = cv2.resize(img,(520,300))               # resize to accelarate the computing speed
+        img = cv2.flip(img, 1)                     # flip right and left
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   # turn BGR into RGB
+        h, w, c = img.shape
 
-    # draw fighters
-    fighter_1.draw(screen)
-    fighter_2.draw(screen)
+        crop_img_1 = img[:, 0:int(w/2)]   # left side person
+        crop_img_2 = img[:, int(w/2):w]   # right side person
+        img.flags.writeable = False
+        
+        results_1 = pose.process(crop_img_1)        # get pose detection result
+        if results_1.pose_landmarks is not None:
+            for i in range(33):
+                results_1.pose_landmarks.landmark[i].x /=2
+                # print(f'person 1: {i} = {results_1.pose_landmarks.landmark[i].x}')
+            left_hand_x, left_hand_y = results_1.pose_landmarks.landmark[15].x, results_1.pose_landmarks.landmark[15].y
+            right_hand_x, right_hand_y = results_1.pose_landmarks.landmark[16].x, results_1.pose_landmarks.landmark[16].y
+            nose_x, nose_y = results_1.pose_landmarks.landmark[0].x, results_1.pose_landmarks.landmark[0].y
+            if l_cnt >= 5:
+                l_cnt = 0
+                # left person raising left hand
+                if left_hand_y < nose_y and action1[0] == False:
+                    action1[0], person_1[0] = True, True
+                    print('left person raising right hand')
+                    # l_cnt = 0
+                elif left_hand_y >= nose_y and action1[0] == True:
+                    action1[0] = False
+                    print('None 0')
+                        
+                # left person raising left hand    
+                elif right_hand_y < nose_y and action1[1] == False:
+                    action1[1], person_1[1] = True, True
+                    print('left person raising left hand')
+                    # l_cnt = 0
+                elif right_hand_y >= nose_y and action1[1] == True:
+                    action1[1] = False
+                    print('None 1')
+                    
+                # left person punch left    
+                elif abs(nose_x - left_hand_x) > PUNCH_TH:  # this num hasn't been tested
+                    if action1[2] == False:
+                        action1[2], person_1[2] = True, True
+                        print('left person punch right')
+                    else:
+                        print('action = punch right, but you should return to None')
 
-    # check for player defeat
-    if round_over == False:
-        if fighter_1.alive == False:
-            score[1] += 1
-            round_over = True
-            round_over_time = pygame.time.get_ticks()
-        elif fighter_2.alive == False:
-            score[0] += 1
-            round_over = True
-            round_over_time = pygame.time.get_ticks()
-    else:
-        # display victory image
-        screen.blit(victory_img, (360, 150))
-        if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
-            round_over = False
-            intro_count = 3
-            fighter_1 = Fighter(1, 200, 310, False, P1_DATA,
-                                warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx, player_s1, HEAD1_DATA)
-            fighter_2 = Fighter(2, 700, 310, True, P2_DATA,
-                                wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx, player_s2, HEAD2_DATA)
+                    # l_cnt = 0
 
-    # event handler
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+                # left person punch right
+                elif abs(right_hand_x - nose_x) > PUNCH_TH:  # this num hasn't been tested
+                    if action1[3] == False:
+                        action1[3], person_1[3] = True, True
+                        print('left person punch left')
+                    else:
+                        print('action = punch left, but you should return to None')
 
-    # update display
-    pygame.display.update()
+                    # l_cnt = 0
+                        
+                # defense
+                elif abs(left_hand_x - right_hand_x) < 0.1:  # this num hasn't been tested
+                    if action1[4] == False:
+                        action1[4], person_1[4] = True, True
+                        print('defense')
+                    else:
+                        print('action = defense, but you should return to None')
+                    
+                # no action   
+                else:
+                    print('None')
+                    action1[2], action1[3], action1[4] = False, False, False
+                    # for i in range(len(action1)):
+                    #     action1[i] = False
+                
+        results_2 = pose.process(crop_img_2)
+        if results_2.pose_landmarks is not None:
+            # print(abs(right_hand_x - nose_x))
+            for i in range(33):
+                results_2.pose_landmarks.landmark[i].x = (results_2.pose_landmarks.landmark[i].x)/2 + 0.5
+                # print(f'person 2: {i} = {results_2.pose_landmarks.landmark[i].x}')
+            left_hand_x, left_hand_y = results_2.pose_landmarks.landmark[15].x, results_2.pose_landmarks.landmark[15].y
+            right_hand_x, right_hand_y = results_2.pose_landmarks.landmark[16].x, results_2.pose_landmarks.landmark[16].y
+            nose_x, nose_y = results_2.pose_landmarks.landmark[0].x, results_2.pose_landmarks.landmark[0].y
+            if r_cnt >= 5:
+                r_cnt = 0
+                # right person raising right hand
+                if left_hand_y < nose_y and action2[0] == False:
+                    action2[0], person_2[0] = True, True
+                    print('right person raising right hand')
+                    # r_cnt = 0
+                elif left_hand_y >= nose_y and action2[0] == True:
+                    action2[0] = False
+                    print('None 0')
+                        
+                # right person raising left hand    
+                elif right_hand_y < nose_y and action2[1] == False:
+                    action2[1], person_2[1] = True, True
+                    print('right person raising left hand')
+                    # r_cnt = 0
+                elif right_hand_y >= nose_y and action2[1] == True:
+                    action2[1] = False
+                    print('None 1')
+                    
+                # right person punch left    
+                elif abs(nose_x - left_hand_x) > PUNCH_TH:  # this num hasn't been tested
+                    if action2[2] == False:
+                        action2[2], person_2[2] = True, True
+                        print('right person punch right')
+                    else:
+                        print('action = punch right, but you should return to None')
+
+                    # r_cnt = 0
+
+                # right person punch right
+                elif abs(right_hand_x - nose_x) > PUNCH_TH:  # this num hasn't been tested
+                    if action2[3] == False:
+                        action2[3], person_2[3] = True, True
+                        print('right person punch left')
+                    else:
+                        print('action = punch left, but you should return to None')
+
+                    # r_cnt = 0
+                        
+                # defense
+                elif abs(left_hand_x - right_hand_x) < 0.1:  # this num hasn't been tested
+                    if action2[4] == False:
+                        action2[4], person_2[4] = True, True
+                        print('defense')
+                    else:
+                        print('action = defense, but you should return to None')
+                    
+                # no action   
+                else:
+                    print('None')
+                    action2[2], action2[3], action2[4] = False, False, False
+                    # for i in range(len(action2)):
+                    #     action2[i] = False
+                        
+
+        sleep(.1)
+
+
+        img.flags.writeable = True
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        # according to pose detection result, label body nodes and skeleton
+        mp_drawing.draw_landmarks(
+            img,
+            results_1.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        mp_drawing.draw_landmarks(
+            img,
+            results_2.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+
+        cv2.imshow('oxxostudio', img)
+        # if cv2.waitKey(5) == ord('q'):
+        #     break     # q to stop
+
+
+        # ⬆︎ human pose estimation ⬆︎
+
+        clock.tick(FPS)
+
+        # draw background
+        draw_bg()
+
+        # show player stats
+        draw_health_bar(fighter_1.health, 20, 20)
+        draw_health_bar(fighter_2.health, 580, 20)
+        draw_text("P1: " + str(score[0]), score_font, RED, 20, 60)
+        draw_text("P2: " + str(score[1]), score_font, RED, 580, 60)
+
+        # update countdown
+        if intro_count <= 0:
+            # move fighters
+            fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT,
+                        screen, fighter_2, round_over, person_1)
+            fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT,
+                        screen, fighter_1, round_over, person_2)
+        else:
+            # display count timer
+            draw_text(str(intro_count), count_font, RED,
+                    SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+            # update count timer
+            if (pygame.time.get_ticks() - last_count_update) >= 1000:
+                intro_count -= 1
+                last_count_update = pygame.time.get_ticks()
+
+        # update fighters
+        fighter_1.update()
+        fighter_2.update()
+
+        # draw fighters
+        fighter_1.draw(screen)
+        fighter_2.draw(screen)
+
+        # check for player defeat
+        if round_over == False:
+            if fighter_1.alive == False:
+                score[1] += 1
+                round_over = True
+                round_over_time = pygame.time.get_ticks()
+            elif fighter_2.alive == False:
+                score[0] += 1
+                round_over = True
+                round_over_time = pygame.time.get_ticks()
+        else:
+            # display victory image
+            screen.blit(victory_img, (360, 150))
+            if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
+                round_over = False
+                intro_count = 3
+                fighter_1 = Fighter(1, 200, 310, False, P1_DATA,
+                                    warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx, player_s1, HEAD1_DATA)
+                fighter_2 = Fighter(2, 700, 310, True, P2_DATA,
+                                    wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx, player_s2, HEAD2_DATA)
+
+        # event handler
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        # update display
+        pygame.display.update()
 
 # exit pygame
 pygame.quit()

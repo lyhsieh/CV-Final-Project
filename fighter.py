442093e4eb2ff59d250620b8ba334619c45a3c9c
Frank-Jhang <frank890725@gmail.com>
2022-12-17 21:15:04 +0800

    feat: integrate with HPE and StyleGAN, add defense front-end, update sprite

--- fighter.py
+++ fighter.py
@@ -2,11 +2,12 @@ import pygame
 
 
 class Fighter():
-    def __init__(self, player, x, y, flip, data, sprite_sheet, animation_steps, sound):
+    def __init__(self, player, x, y, flip, data, sprite_sheet, animation_steps, sound, style_img_list, head_data):
         self.player = player
-        self.size = data[0]
-        self.image_scale = data[1]
-        self.offset = data[2]
+        self.size_x = data[0]
+        self.size_y = data[1]
+        self.image_scale = data[2]
+        self.offset = data[3]
         self.flip = flip
         self.animation_list = self.load_images(sprite_sheet, animation_steps)
         self.action = 0  # 0:idle #1:run #2:jump #3:attack1 #4: attack2 #5:hit #6:death
@@ -18,12 +19,21 @@ class Fighter():
         self.running = False
         self.jump = False
         self.attacking = False
+        self.defense = False
         self.attack_type = 0
         self.attack_cooldown = 0
         self.attack_sound = sound
         self.hit = False
         self.health = 100
         self.alive = True
+        self.head_list = style_img_list
+        self.head_count = 0
+        self.head_size = head_data[0]
+        self.head_scale = head_data[1]
+        self.head_offset = head_data[2]
+        self.head_img = pygame.image.load(self.head_list[self.head_count])   # start from the first style img (0)
+        self.head_img = pygame.transform.scale(self.head_img, (self.head_size, self.head_size))
+
 
     def load_images(self, sprite_sheet, animation_steps):
         # extract images from spritesheet
@@ -32,13 +42,13 @@ class Fighter():
             temp_img_list = []
             for x in range(animation):
                 temp_img = sprite_sheet.subsurface(
-                    x * self.size, y * self.size, self.size, self.size)
+                    x * self.size_x, y * self.size_y, self.size_x, self.size_y)
                 temp_img_list.append(pygame.transform.scale(
-                    temp_img, (self.size * self.image_scale, self.size * self.image_scale)))
+                    temp_img, (self.size_x * self.image_scale, self.size_y * self.image_scale)))
             animation_list.append(temp_img_list)
         return animation_list
 
-    def move(self, screen_width, screen_height, surface, target, round_over):
+    def move(self, screen_width, screen_height, surface, target, round_over, action):
         SPEED = 10
         GRAVITY = 2
         dx = 0
@@ -54,10 +64,10 @@ class Fighter():
             # check player 1 controls
             if self.player == 1:
                 # movement
-                if key[pygame.K_a]:
+                if key[pygame.K_a] or action[0]:    # raise left hand
                     dx = -SPEED
                     self.running = True
-                if key[pygame.K_d]:
+                if key[pygame.K_d] or action[1]:    # raise right hand
                     dx = SPEED
                     self.running = True
                 # jump
@@ -65,21 +75,24 @@ class Fighter():
                     self.vel_y = -30
                     self.jump = True
                 # attack
-                if key[pygame.K_r] or key[pygame.K_t]:
+                if key[pygame.K_r] or key[pygame.K_t] or action[2] or action[3]:    # punch left or right
                     self.attack(target)
                     # determine which attack type was used
-                    if key[pygame.K_r]:
-                        self.attack_type = 1
-                    if key[pygame.K_t]:
+                    if key[pygame.K_r] or action[2] or action[3]:
                         self.attack_type = 2
+                    if key[pygame.K_t] or action[2] or action[3]:
+                        self.attack_type = 1
+                # defense
+                if key[pygame.K_f] or action[4]:    # defense
+                    self.defense = True
 
             # check player 2 controls
             if self.player == 2:
                 # movement
-                if key[pygame.K_LEFT]:
+                if key[pygame.K_LEFT] or action[0]:
                     dx = -SPEED
                     self.running = True
-                if key[pygame.K_RIGHT]:
+                if key[pygame.K_RIGHT] or action[1]:
                     dx = SPEED
                     self.running = True
                 # jump
@@ -87,13 +100,16 @@ class Fighter():
                     self.vel_y = -30
                     self.jump = True
                 # attack
-                if key[pygame.K_SPACE] or key[pygame.K_KP2]:
+                if key[pygame.K_SPACE] or key[pygame.K_KP2] or action[2] or action[3]:
                     self.attack(target)
                     # determine which attack type was used
-                    if key[pygame.K_SPACE]:
-                        self.attack_type = 1
-                    if key[pygame.K_KP2]:
+                    if key[pygame.K_SPACE] or action[2] or action[3]:
                         self.attack_type = 2
+                    if key[pygame.K_KP2] or action[2] or action[3]:
+                        self.attack_type = 1
+                # defense
+                if key[pygame.K_m] or action[4]:    # defense
+                    self.defense = True
 
         # apply gravity
         self.vel_y += GRAVITY
@@ -142,8 +158,11 @@ class Fighter():
             self.update_action(2)  # 2:jump
         elif self.running == True:
             self.update_action(1)  # 1:run
+        elif self.defense == True:
+            self.update_action(7)   # 7:defense
         else:
             self.update_action(0)  # 0:idle
+
 
         animation_cooldown = 50
         # update image
@@ -169,6 +188,9 @@ class Fighter():
                     # if the player was in the middle of an attack, then the attack is stopped
                     self.attacking = False
                     self.attack_cooldown = 20
+                if self.action == 7:
+                    self.defense = False
+
 
     def attack(self, target):
         if self.attack_cooldown == 0:
@@ -180,6 +202,9 @@ class Fighter():
             if attacking_rect.colliderect(target.rect):
                 target.health -= 10
                 target.hit = True
+                target.head_count += 1
+                target.head_img = pygame.image.load(target.head_list[target.head_count])   # start from the first style img (0)
+                target.head_img = pygame.transform.scale(target.head_img, (target.head_size, target.head_size))
 
     def update_action(self, new_action):
         # check if the new action is different to the previous one
@@ -191,5 +216,8 @@ class Fighter():
 
     def draw(self, surface):
         img = pygame.transform.flip(self.image, self.flip, False)
+        head = pygame.transform.flip(self.head_img, self.flip, False)
         surface.blit(img, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (
             self.offset[1] * self.image_scale)))
+        surface.blit(head, (self.rect.x - (self.head_offset[0] * self.head_scale), self.rect.y - (
+            self.head_offset[1] * self.head_scale)))
